d8d16e82bc861817bea937e3c6fa2413f067b561
Syed Amaanuddin <amaanuddinsyed2005@gmail.com>
2026-06-26 00:24:55 +0530

    feat: added advance, peak, addToken, match helper functions for the scanTokens function

--- include/scanner.hpp
+++ include/scanner.hpp
@@ -1,16 +1,37 @@
 #pragma once
 
-#include "token.hpp"
 #include <string>
 #include <vector>
+
+#include "token.hpp"
+#include "tokenType.hpp"
 
 namespace lox {
 
 class Scanner {
+private:
+  size_t start = 0;
+  size_t current = 0;
+  int lineNumber = 1;
+
   std::string sourceCode;
   std::vector<Token> tokens;
-    public:
-      explicit Scanner(const std::string &source);
-      std::vector<Token> scanTokens();
-  };
-}
+
+  char advance(); // this function returns the character to be scanned and moves
+                  // the current pointer to next character
+  char peek(); // the peek function only returns the character present
+                    // next to the character that is being currently scanned.
+  void
+  addToken(TokenType type,
+           Literal literal); // addToken function is responsible for filling the
+                             // tokens vectors with the Token information.
+  bool match(const char expected); // the match looks at the next character to determine if it is a two character token like(>=, ==, <=, +=......)
+  
+public:
+  explicit Scanner(const std::string &source);
+  // the scanTokens function scans all the lexemes and map them to their
+  // corresponding tokentype as defined in the enum class TokenType
+  std::vector<Token> scanTokens();
+};
+
+} // namespace lox
--- src/scanner.cpp
+++ src/scanner.cpp
@@ -1,12 +1,38 @@
 #include "scanner.hpp"
 #include "token.hpp"
-#include <iostream>
+#include <iterator>
 #include <vector>
 
 lox::Scanner::Scanner(const std::string &source) { this->sourceCode = source; }
 
 std::vector<lox::Token> lox::Scanner::scanTokens() {
-  std::cout << "************Source Code************" << std::endl;
-  std::cout << sourceCode << std::endl;
+  // std::cout << "************Source Code************" << std::endl;
+  // std::cout << sourceCode << std::endl;
+
+  while (current < sourceCode.length()) {
+    char c = advance();
+    switch (c) {}
+  }
+
   return tokens;
+}
+
+char lox::Scanner::advance() { return sourceCode[current++]; }
+
+char lox::Scanner::peek() {
+  if (current >= sourceCode.length()) {
+    return '\0';
+  }
+  return sourceCode[current];
+}
+
+bool lox::Scanner::match(const char expected) {
+  if (current >= sourceCode.length() || sourceCode[current] != expected)
+    return false;
+  current++;
+  return true;
+}
+
+void lox::Scanner::addToken(TokenType type, Literal literal) {
+  
 }
